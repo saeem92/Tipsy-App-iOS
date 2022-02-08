3d590e963f2b412d755e2bae0c24c35ac7c9796f
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-08 12:12:20 +0530

    changes

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -16,14 +16,30 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    var tip = 0.10
     
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        // We are adding isselected here it is helping us to have a background and appear selected when we press a button.
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        //Make the button that triggered the IBAction selected.
+                sender.isSelected = true
+                
+                //Get the current title of the button that was pressed.
+                let buttonTitle = sender.currentTitle!
+                
+                //Remove the last character (%) from the title then turn it back into a String.
+                let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+                
+                //Turn the String into a Double.
+                let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+                
+                //Divide the percent expressed out of 100 into a decimal e.g. 10 becomes 0.1
+                tip = buttonTitleAsANumber / 100
+        
     }
     
     
