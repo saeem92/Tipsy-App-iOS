b6a1fb661a8b37830d7590c904717dfa12ba4ec9
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-09 02:02:18 +0530

    Complete App

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -17,9 +17,17 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var billTextField: UITextField!
     
     var tip = 0.10
+    // Initial start of the tip
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
     
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+               billTextField.endEditing(true)
+        //Dismiss the keyboard when the user chooses one of the tip values.
+        
         zeroPctButton.isSelected = false
         // We are adding isselected here it is helping us to have a background and appear selected when we press a button.
         tenPctButton.isSelected = false
@@ -44,10 +52,38 @@ class CalculatorViewController: UIViewController {
     
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+               splitNumberLabel.text = String(format: "%.0f", sender.value)
+        //Get the stepper value using sender.value, round it down to a whole number using String(format: "%.0f", sender.value) then set it as the text in splitNumberLabel
+               
+               numberOfPeople = Int(sender.value)
+        //Set the numberOfPeople property as the value of the stepper as a whole number.
     }
     
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        
+        //Get the text the user typed in the billTextField
+             let bill = billTextField.text!
+             
+             //If the text is not an empty String ""
+             if bill != "" {
+                 
+                 //Turn the bill from a String e.g. "123.50" to an actual String with decimal places.
+                 //e.g. 125.50
+                 billTotal = Double(bill)!
+                 
+                 //Multiply the bill by the tip percentage and divide by the number of people to split the bill.
+                 let result = billTotal * (1 + tip) / Double(numberOfPeople)
+                 
+                 //Round the result to 2 decimal places and turn it into a String.
+                 let resultTo2DecimalPlaces = String(format: "%.2f", result)
+                 finalResult = String(format: "%.2f", result)
+             }
+        //In Main.storyboard there is a segue between CalculatorVC and ResultsVC with the identifier "goToResults".
+               //This line triggers the segue to happen.
+               self.performSegue(withIdentifier: "goToResults", sender: self)
     }
     
     
